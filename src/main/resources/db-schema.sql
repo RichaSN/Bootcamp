ba1211c627b43c7c3e07096621c8c7cb35c909cd
Richa S <richa.shrivastava@hyperisland.se>
2021-05-13 13:41:34 +0200

    initial commits for skeleton project

--- src/main/resources/db-schema.sql
+++ src/main/resources/db-schema.sql
@@ -1,8 +1,3 @@
-# Bootcamp
-
-create database  SALES in mysql
-
-use sales database and create below table
 CREATE TABLE `product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
